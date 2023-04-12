076f2fcb57faf95c3de1ff00d7a91dbf1add9f4f
DrDAN6770 <118630187+DrDAN6770@users.noreply.github.com>
2023-04-12 22:06:06 +0800

    Add files via upload

---
+++ Ch05/Program5_2.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// %¨ú¾l¼Æ
+
+int main(void)
+{
+	printf("12%%4=%d\n",12%4);
+	printf("12%%5=%d\n", 12%5);
+	printf("12%%16=%d\n", 12%16);
+	return 0;
+}
+
+//12 % 4 = 0
+//12 % 5 = 2
+//12 % 16 = 12
