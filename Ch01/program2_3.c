260aae73ef2f44a87323e9321aa9d28b30b8ac90
DrDAN6770 <118630187+DrDAN6770@users.noreply.github.com>
2023-04-12 15:48:03 +0800

    Rename program2_3.c to Ch01/program2_3.c

---
+++ Ch01/program2_3.c
@@ -0,0 +1,21 @@
+/* ¸ê®Æ«¬ºA */
+/*
+char ¦r¤¸
+int ¾ã¼Æ
+long ªø¾ã¼Æ
+short µu¾ã¼Æ
+float ¯BÂI¼Æ
+double ºë½T¯BÂI¼Æ
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int num = 2;
+	printf("I have %d cats. \n", num);
+	return 0;
+}
+
+//I have 2 cats.
